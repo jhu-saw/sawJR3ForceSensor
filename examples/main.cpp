80fd39caf7802a69a3ab0a63af54d8f11bd1811b
Min Yang Jung <myj@jhu.edu>
2012-03-27 02:44:18 +0000

    sawJR3ForceSensor: Fixed typo
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk/saw/components/sawJR3ForceSensor@3575 0959eb1a-6956-4f60-947b-6092a4e22c97

--- examples/main.cpp
+++ examples/main.cpp
@@ -73,7 +73,7 @@ int main()
 
     mtsTaskManager * taskManager = mtsTaskManager::GetInstance();
 
-#if (CISST_OS == CISST_LINUX_XENOMA) || (CISST_OS == CISST_LINUX)
+#if (CISST_OS == CISST_LINUX_XENOMAI) || (CISST_OS == CISST_LINUX)
     mtsJR3ForceSensor * jr3 = new mtsJR3ForceSensor("JR3", "/dev/comedi4", 0);
 #elif (CISST_OS == CISST_QNX)
     mtsJR3ForceSensor * jr3 = new mtsJR3ForceSensor("JR3");
